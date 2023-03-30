8f73bc853c84902a834c0a1f2b533674db29d498
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-30 15:51:53 -0400

    add: map of api to render images

--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -1,4 +1,15 @@
+import { useContext } from "react";
+import { Context } from "../components/Context";
+import Image from "../components/Image";
+import { getClass } from "../utils";
+
 function Photos() {
-  return <h1>Photos go here</h1>;
+  const { allPhotos } = useContext(Context);
+
+  const imageElements = allPhotos.map((img, index) => (
+    <Image key={img.id} img={img} className={getClass(index)} />
+  ));
+
+  return <main className="photos">{imageElements}</main>;
 }
 export default Photos;

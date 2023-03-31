bbd69ee4dfd99530d0436d9a8787b26c9505f356
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-31 15:46:19 -0400

    add: prop types & conditionals

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,8 +1,10 @@
 import { useContext, useState } from "react";
 import { Context } from "./Context";
+import PropTypes from "prop-types";
 
 function Image({ className, img }) {
-  const { toggleFavorite } = useContext(Context);
+  const { toggleFavorite, cartItems, addToCart, removeFromCart } =
+    useContext(Context);
   const [hovered, setHovered] = useState(false);
 
   const heartIcon = hovered && (
@@ -17,7 +19,24 @@ function Image({ className, img }) {
       onClick={() => toggleFavorite(img.id)}></i>
   );
 
-  const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+  function cartIcons() {
+    const inCart = cartItems.some((item) => item.id === img.id);
+    if (inCart) {
+      return (
+        <i
+          className="ri-shopping-cart-fill cart"
+          onClick={() => removeFromCart(img.id)}></i>
+      );
+    } else if (hovered) {
+      return (
+        <i
+          className="ri-add-circle-line cart"
+          onClick={() => {
+            addToCart(img);
+          }}></i>
+      );
+    }
+  }
 
   return (
     <div
@@ -25,10 +44,20 @@ function Image({ className, img }) {
       onMouseOver={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}>
       <img src={img.url} className="image-grid" />
+
       {img.isFavorite ? heartFillIcon : heartIcon}
-      {cartIcon}
+      {cartIcons()}
     </div>
   );
 }
+
+Image.propTypes = {
+  className: PropTypes.string,
+  img: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    isFavorite: PropTypes.bool.isRequired,
+  }).isRequired,
+};
 
 export default Image;

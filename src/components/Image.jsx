b486fbf8b9248648e321299db1d94b397677bfae
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-31 12:53:27 -0400

    add: function and conditionals

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,16 +1,31 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
+import { Context } from "./Context";
 
 function Image({ className, img }) {
+  const { toggleFavorite } = useContext(Context);
   const [hovered, setHovered] = useState(false);
-  const heartIcon = hovered && <i className="ri-heart-line favorite"></i>;
+
+  const heartIcon = hovered && (
+    <i
+      className="ri-heart-line favorite"
+      onClick={() => toggleFavorite(img.id)}></i>
+  );
+
+  const heartFillIcon = (
+    <i
+      className="ri-heart-fill favorite"
+      onClick={() => toggleFavorite(img.id)}></i>
+  );
+
   const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>;
+
   return (
     <div
       className={`${className} image-container`}
       onMouseOver={() => setHovered(true)}
       onMouseLeave={() => setHovered(false)}>
       <img src={img.url} className="image-grid" />
-      {heartIcon}
+      {img.isFavorite ? heartFillIcon : heartIcon}
       {cartIcon}
     </div>
   );

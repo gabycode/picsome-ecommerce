bfad852e2cc0110b9945fadc3e124496687a965a
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-31 23:31:27 -0400

    add: conditionals for cart icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,22 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "./Context";
 
 function Header() {
+  const { cartItems } = useContext(Context);
+  const cartIcon =
+    cartItems.length > 0 ? (
+      <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+    ) : (
+      <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+    );
+
   return (
     <header>
       <h2>
         <Link to="/picsome-ecommerce/">Pic Some</Link>
       </h2>
-      <Link to="/cart">
-        <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
-      </Link>
+      <Link to="/cart">{cartIcon}</Link>
     </header>
   );
 }

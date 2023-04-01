c57a097daaf87c4697745c4bbb92a2ddd0b39d48
gabycode <86427446+gabycode@users.noreply.github.com>
2023-03-31 23:31:02 -0400

    add: place order and conditionals

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,4 +1,42 @@
+import { useContext, useState } from "react";
+import { Context } from "../components/Context";
+import CartItem from "../components/CartItem";
+
 function Cart() {
-  return <h1>Cart goes here</h1>;
+  const { cartItems, emptyCart } = useContext(Context);
+  const [buttonText, setButtonText] = useState("Place Order");
+  const cartItemsCost = cartItems.length * 5.99;
+  const totalCostOfCart = cartItemsCost.toLocaleString("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  const cartItemElements = cartItems.map((item) => (
+    <CartItem key={item.id} item={item} />
+  ));
+
+  function placeOrder() {
+    setButtonText("Ordering...");
+    setTimeout(() => {
+      setButtonText("Place Order");
+      emptyCart();
+      console.log("order placed!");
+    }, 3000);
+  }
+
+  return (
+    <div>
+      <h1>Cart goes here</h1>
+      {cartItemElements}
+      <p className="total-cost">Total: {totalCostOfCart}</p>
+      {cartItems.length > 0 ? (
+        <div className="order-button">
+          <button onClick={placeOrder}>{buttonText}</button>
+        </div>
+      ) : (
+        <p>No items in cart</p>
+      )}
+    </div>
+  );
 }
 export default Cart;
